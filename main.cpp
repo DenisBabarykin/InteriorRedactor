ff7e6e35a2fea8d091ed215f1497793501d0c4a7
babarykin_denis <babarykin.denis@gmail.com>
2014-12-12 20:27:41 +0300

    Установлен стиль qdarkstyle.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainForm w;
+
+    // Установка стиля qdarkstyle
+    QFile f(":qdarkstyle/style.qss");
+    if (f.exists())
+    {
+        f.open(QFile::ReadOnly | QFile::Text);
+        QTextStream ts(&f);
+        a.setStyleSheet(ts.readAll());
+    }
+
     w.show();
     w.MoveFrameToCenter(); // Центрируем форму
 
